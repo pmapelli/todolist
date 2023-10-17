30b558bad9c63c48382c5972b3d79666663c1a70
Pedro Nunes Mapelli <pedro-nm@detran.go.gov.br>
2023-10-17 17:41:48 -0300

    chore: First commit

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#131016',
+        padding: 24
+    },
+    header: {
+        alignContent: 'flex-start',
+        flex: 1,
+        backgroundColor: '#0D0D0D',        
+    }
+})
